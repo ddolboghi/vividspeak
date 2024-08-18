6b535e7527d3fa3d8d568d2b2c0ff1089a60498e
ddolboghi <rhdfyd128@gmail.com>
2024-08-18 17:27:20 +0900

    fix: 화면 가운데 정렬

--- src/components/Introduce.tsx
+++ src/components/Introduce.tsx
@@ -1,3 +1,7 @@
 export default function Introduce() {
-  return <div className="m-2">이제 자신있게 영어로 말하세요!</div>;
+  return (
+    <div className="flex items-center justify-center m-2">
+      이제 자신있게 영어로 말하세요!
+    </div>
+  );
 }
