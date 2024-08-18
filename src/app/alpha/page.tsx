332714a90e78ccda16f0056e6a88effa4b2acf37
ddolboghi <rhdfyd128@gmail.com>
2024-08-18 16:43:14 +0900

    feat: 헤더 로고 추가

--- src/app/alpha/page.tsx
+++ src/app/alpha/page.tsx
@@ -1,9 +1,14 @@
-import Introduce from "@/components/Introduce";
-import RegisterForm from "@/components/RegisterForm";
+import Image from "next/image";
+import vividLogo from "@/public/vividLogo.svg";
+import Introduce from "@/src/components/Introduce";
+import RegisterForm from "@/src/components/RegisterForm";
 
 const Page: React.FC = () => {
   return (
     <main>
+      <header className="p-3 flex justify-center">
+        <Image src={vividLogo} width={480} height={120} alt="vivid logo" />
+      </header>
       <Introduce />
       <RegisterForm />
     </main>
