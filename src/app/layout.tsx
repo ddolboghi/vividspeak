68ea54f9d9dd31c61c07c8a4255022e786656942
ddolboghi <rhdfyd128@gmail.com>
2024-08-18 21:12:19 +0900

    feat: 폰트 전역 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,20 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import localFont from "next/font/local";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const pretendard = localFont({
+  src: "../../public/PretendardVariable.woff2",
+  display: "swap",
+  weight: "45 920",
+  variable: "--font-pretendard",
+});
+
 export const metadata: Metadata = {
   title: "VIVID SPEAK",
-  description: "1대1 영어 회화 과외",
+  description: "1대1 영어회화 과외",
   icons: {
     icon: "/vividone.svg",
   },
@@ -18,8 +26,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="kr" className={`${pretendard.variable}`}>
+      <body className={pretendard.className}>{children}</body>
     </html>
   );
 }
