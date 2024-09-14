2f87126b78a3ffb52a60f699e921382ac12a7dcd
ddolboghi <rhdfyd128@gmail.com>
2024-09-14 13:59:38 +0900

    feat: 질문 데이터 추가

---
+++ src/data/question.ts
@@ -0,0 +1,27 @@
+export const questions = [
+  {
+    id: 1,
+    question: "10대 인가요?",
+    score: 3,
+  },
+  {
+    id: 2,
+    question: "씻을 때마다 새 수건을 사용하나요?",
+    score: 7,
+  },
+  {
+    id: 3,
+    question: "일주일에 5번 이상 인스턴트 식품을 먹나요?",
+    score: 5,
+  },
+  {
+    id: 4,
+    question: "단 간식(사탕, 초콜릿, 젤리 등)을 매일 먹나요?",
+    score: 7,
+  },
+  {
+    id: 5,
+    question: "세수하고 나서 2시간안에 기름이 올라오는 편인가요?",
+    score: 8,
+  },
+];
