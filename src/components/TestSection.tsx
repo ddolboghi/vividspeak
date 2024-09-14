987115a5ff2ab81a1a16225c782ef6170ac4d564
ddolboghi <rhdfyd128@gmail.com>
2024-09-14 14:00:11 +0900

    refactor: 질문 페이지 로직 수정

--- src/components/TestSection.tsx
+++ src/components/TestSection.tsx
@@ -2,25 +2,63 @@
 
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { questions } from "../data/question";
 
 export default function TestSection() {
   const route = useRouter();
-  const [inputValue, setInputValue] = useState("");
+  const [answers, setAnswers] = useState<{ [key: number]: number }>({});
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0); // 현재 질문 인덱스 상태
+
+  const handleChange = (id: number, value: number) => {
+    setAnswers((prev) => ({ ...prev, [id]: value }));
+    setCurrentQuestionIndex((prev) => prev + 1);
+  };
 
   const handleClickResultBtn = () => {
-    route.push(`/ilovetest/${inputValue}`);
+    if (Object.keys(answers).length !== questions.length) {
+      alert("모든 질문에 답변해 주세요.");
+      return;
+    }
+
+    const totalScore = Object.values(answers).reduce(
+      (acc, score) => acc + score,
+      0
+    );
+    route.push(`/ilovetest/${totalScore}`);
   };
 
   return (
     <div>
-      TestSection
-      <input
-        placeholder="결과를 입력해주세요"
-        type="text"
-        value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
-      />
-      <button onClick={handleClickResultBtn}>결과 보기</button>
+      {currentQuestionIndex < questions.length && (
+        <div key={questions[currentQuestionIndex].id}>
+          <p>{questions[currentQuestionIndex].question}</p>
+          <div className="flex flex-row gap-4">
+            <button
+              onClick={() =>
+                handleChange(
+                  questions[currentQuestionIndex].id,
+                  questions[currentQuestionIndex].score
+                )
+              }
+            >
+              예
+            </button>
+            <button
+              onClick={() =>
+                handleChange(
+                  questions[currentQuestionIndex].id,
+                  10 - questions[currentQuestionIndex].score
+                )
+              }
+            >
+              아니오
+            </button>
+          </div>
+        </div>
+      )}
+      {currentQuestionIndex === questions.length && (
+        <button onClick={handleClickResultBtn}>결과 보기</button>
+      )}
     </div>
   );
 }
